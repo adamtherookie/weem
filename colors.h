c1bb960a36388df14202d8caafb9a39362b60ed1
AtiPique_ <46247526+AtiPiquee@users.noreply.github.com>
2023-08-14 13:08:33 +0200

    Update colors.h

--- colors.h
+++ colors.h
@@ -20,23 +20,4 @@
 #define color14 0xf3dab1 // #f3dab1
 #define color15 0xf3f3f3 // #f3f3f3
 
-// Nord color scheme
-
-#define nord0  0x2E3440 // #2E3440
-#define nord1  0x3B4252 // #3B4252
-#define nord2  0x434C5E // #434C5E
-#define nord3  0x4C566A // #4C566A
-#define nord4  0xD8DEE9 // #D8DEE9
-#define nord5  0xE5E9F0 // #E5E9F0
-#define nord6  0xECEFF4 // #ECEFF4
-#define nord7  0x8FBCBB // #8FBCBB
-#define nord8  0x88C0D0 // #88C0D0
-#define nord9  0x81A1C1 // #81A1C1
-#define nord10 0x5E81AC // #5E81AC
-#define nord11 0xbf616a // #bf616a
-#define nord12 0xd08770 // #d08770
-#define nord13 0xebcb8b // #ebcb8b
-#define nord14 0xa3be8c // #a3be8c
-#define nord15 0xb48ead // #b48ead
-
 #endif
